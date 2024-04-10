2a1ec07f791e91179de75b2e349276a6e5300518
ChrisferDev <ferchris82@hotmail.com>
2024-04-10 16:11:21 -0500

    Versión 1.1

--- src/main/java/chrisferdev/beststore/controllers/ProductsController.java
+++ src/main/java/chrisferdev/beststore/controllers/ProductsController.java
@@ -6,31 +6,65 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 import chrisferdev.beststore.models.Product;
 import chrisferdev.beststore.models.ProductDto;
 import chrisferdev.beststore.services.ProductsRepository;
+import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequestMapping("/products")
 public class ProductsController {
-    
+
     @Autowired
     private ProductsRepository repo;
 
-    @GetMapping({"", "/"})
-    public String showProductList(Model model){
+    @GetMapping({ "", "/" })
+    public String showProductList(Model model) {
         List<Product> products = repo.findAll(Sort.by(Sort.Direction.DESC, "id"));
         model.addAttribute("products", products);
         return "products/index";
     }
 
     @GetMapping("/create")
-    public String showCreatePage(Model model){
+    public String showCreatePage(Model model) {
         ProductDto productDto = new ProductDto();
         model.addAttribute("productDto", productDto);
         return "products/CreateProduct";
-    } 
+    }
+
+    @PostMapping("/create")
+    public String createProduct(
+        @Valid @ModelAttribute ProductDto productDto, BindingResult result) {
+
+        if (productDto.getImageFile().isEmpty()) {
+            result.addError(new FieldError("productDto", "imageFile", "The image file is required"));
+        }
+
+        if (result.hasErrors()) {
+            return "products/CreateProduct";
+        }
+        return "redirect:/products";
+
+    // save image file
+    MultipartFile image = productDto.getImageFile();
+    Date createdAt = new Date();
+    String storageFileName = createdAt.getTime() + "_" + image.getOriginalFilename();
+
+    try {
+        String uploadDir = "public/images/";
+        Path
+    } catch (Exception ex){
+
+    }
+
+    }
 }
